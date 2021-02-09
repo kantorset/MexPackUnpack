ae73ebfc95a06708388b5ec3dae5f6907a8aa210
none <noreply>
2021-02-08 20:08:04 -0500

    Fixed typo in example

--- examples/example_2_interleaved.cpp
+++ examples/example_2_interleaved.cpp
@@ -19,7 +19,7 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
     auto [a, b, c, d] = my_unpack.unpackMex();
     auto [d_p, d_M, d_N] = d; //dp is std::complex<double>* (pointer to complex data), d_M is number of rows, d_N is number of columns
     
-    Eigen::MatrixXcd e_comp = b*a;
+    Eigen::MatrixXcd e_comp = b*c;
 
     MexPacker<std::complex<double>, int, Eigen::MatrixXcd, CDIP> my_pack(nlhs, plhs);
     my_pack.PackMex(b, 2, e_comp, d);
