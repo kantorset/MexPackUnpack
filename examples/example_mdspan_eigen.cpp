08552cf775e51236db7de73693f34e586ff37bde
none <None@None.com>
2022-02-27 08:19:49 -0500

    eigen mdspan example

---
+++ examples/example_mdspan_eigen.cpp
@@ -0,0 +1,37 @@
+#include "MexPackUnpack.h"
+#include "mex.h"
+#include <Eigen>
+#include <iostream>
+
+using namespace MexPackUnpackTypes;
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[]) {
+
+  // Create an unpacker with template parameters corresponding to what we expect from matlab
+  // argument 1: 4 dimensional mdspan
+
+  MexUnpacker<span_4d_dynamic_left<double>  > my_unpack(nrhs, prhs); //We need the pointers to the inputs from matlab/octave
+
+  try {
+
+    auto [a] = my_unpack.unpackMex(); 
+    if(a.extent(1)<4||a.extent(2)<3||a.extent(3)<4){
+      throw std::string("Input is too small for slice indices");
+    }
+
+    double b = a(0,1,2,3);
+
+    auto c = stdex::submdspan(a,1,std::pair{2ul,4ul},stdex::full_extent,3); //Create a slice (submdspan)
+
+    Eigen::Map<Eigen::MatrixXd,0,Eigen::Stride<Eigen::Dynamic,Eigen::Dynamic> > d(c.data(),c.extent(0),c.extent(1),Eigen::Stride<Eigen::Dynamic,Eigen::Dynamic>(c.stride(1),c.stride(0)));
+
+    Eigen::MatrixXd e = d+d;
+
+    MexPacker<double , decltype(c),Eigen::MatrixXd > my_pack(nlhs, plhs); //Create a packing object, we use decltype to get the strides from submdspan correct
+    my_pack.PackMex(b,c,e); //Return this object to matlab
+
+  } catch (std::string s) {
+    mexPrintf(s.data());
+  }
+  
+}
